95e4fb37844ce5f4ea465034f488d17555746b92
Scott Benson <61036304+sbenson189@users.noreply.github.com>
2021-06-02 17:38:06 -0500

    Animation added to OrderSubmitted View. Assets for animation added.

--- StoreAppProject/StoreAppProject/OrderSubmittedViewController.swift
+++ StoreAppProject/StoreAppProject/OrderSubmittedViewController.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class OrderSubmittedViewController: UIViewController {
-
+    @IBOutlet weak var truck: UIImageView!
+    @IBOutlet weak var box: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        boxAnimateUp()
+        
+        func boxAnimateUp() {
+            UIView.animateKeyframes(withDuration: 0.7, delay: 0, animations: {
+                self.box.transform = CGAffineTransform(translationX: 0, y: -50)
+            }, completion: {_ in boxAnimateRight()})
+        }
+        
+        func boxAnimateRight() {
+            UIView.animateKeyframes(withDuration: 0.7, delay: 0, animations: {
+                self.box.transform = CGAffineTransform(translationX: 90, y: -50)
+                self.box.alpha = 0
+            }, completion: {_ in truckAnimateRight()})
+        }
+        
+        func truckAnimateRight() {
+            UIView.animateKeyframes(withDuration: 1, delay: 0, animations: {
+                self.truck.transform = CGAffineTransform(translationX: 400, y: 0)
+            }, completion: {_ in truckAnimateLeft()})
+        }
+        
+        func truckAnimateLeft() {
+            UIView.animateKeyframes(withDuration: 1, delay: 0, animations: {
+                self.truck.transform = CGAffineTransform(translationX: 0, y: 0)
+            }, completion: {_ in boxAnimateLeft()})
+        }
+        
+        func boxAnimateLeft() {
+            UIView.animateKeyframes(withDuration: 0.7, delay: 0, animations: {
+                self.box.transform = CGAffineTransform(translationX: 0, y: -50)
+                self.box.alpha = 1
+            }, completion: {_ in boxAnimateDown()})
+        }
+        
+        func boxAnimateDown() {
+            UIView.animateKeyframes(withDuration: 0.7, delay: 0, animations: {
+                self.box.transform = CGAffineTransform(translationX: 0, y: 0)
+            }, completion: {_ in boxAnimateUp()})
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
